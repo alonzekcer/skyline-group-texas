93d34e5c73fa0678e9fee8ac348d06f47d6b37a4
אלון זקסר <alonzekcer@MacBookAir.home>
2025-12-21 20:49:12 +0200

    Build: Deploy from docs folder on main branch

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: process.env.GITHUB_PAGES ? '/skyline-group-texas/' : '/',
+    base: mode === 'production' ? '/skyline-group-texas/' : '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
+    },
+    build: {
+      outDir: 'docs',
+      emptyOutDir: true,
     },
     plugins: [react()],
     define: {
